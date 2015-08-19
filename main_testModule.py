dbff976337f59e48c83e5b97f422b0a5fa9a5bb0
AyaZhang <yiz160@ucsd.edu>
2015-08-18 17:07:12 -0700

    linear regression

--- main_testModule.py
+++ main_testModule.py
@@ -12,41 +12,46 @@ shoes = parseData.parse(pathProducts)
 reviews = parseData.parse(pathReviews)
 labels = parseData.loadLabels(pathLabels)
 
-sample_shoes = list()
+sample_shoes = dict()
 for i in shoes:
   if i['asin'] in labels.keys():
-    sample_shoes.append(i)
+    sample_shoes[i['asin']] = i
 
 sample_reviews = list()
 for i in reviews:
   if i['asin'] in labels.keys():
     sample_reviews.append(i)
 
-def fearure(datum):
-  feat = [0] * len()
-
-y = [labels[r['asin']] for r in sample_reviews]
-
-
-#########
-bgs = [x[1] for x in bigramWordCount[:1000]]
-bgsId = dict(zip(bgs, range(len(bgs))))
-bgsSet = set(bgs)
-
-def feature(datum):
-  feat = [0] * len(bgs)
-  r = ''.join([c for c in datum['review/text'].lower() if not c in punctuation])
-  r = r.replace('\t', ' ')
-  words = r.split()
-  for i in range(len(words)-1):
-    bg = words[i] + " " + words[i+1]
-    if bg in bgs:
-      feat[bgsId[bg]] += 1
-  feat.append(1) #offset
+def feature(key):
+  feat = [1]
+  feat.append(sample_shoes[key]['price'])  # price feature
+  count = 0
+  for i in sample_reviews:
+    if i['asin'] == key:
+      count += 1
+  feat.append(count) # popularity featur
   return feat
 
-X = [feature(d) for d in data]
-y = [d['review/overall'] for d in data]
+d1 = dict(sample_shoes.items()[len(sample_shoes)/2:]) # training set
+d2 = dict(sample_shoes.items()[:len(sample_shoes)/2]) # test set
+
+y = [labels[key] for key in d1]
+X = [feature(key) for key in d1]
 
 theta,residuals,rank,s = numpy.linalg.lstsq(X, y)
-residuals
+
+Xi = [feature(key) for key in d1]
+
+def mse(x, y, theta0, theta1):
+  total = 0
+  for i in range(0,len(x)):
+    total += (y[i] - x[i] * theta1 - theta0) ** 2
+  return total/len(x)
+
+mse(Xi, y, theta[0], theta[1])
+
+# What is the model’s MSE on the test set?
+Xi = [feature(key) for key in d2]
+y = [labels[key] for key in d2]
+
+mse(Xi, y, theta[0], theta[1])  
